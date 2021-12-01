6cf933a6b57cee7d24b12b08086f246224514c12
couturierdea <couturierdea@gmail.com>
2021-12-01 14:56:57 +0200

    Рефакторинг с использованием rx-store, rx-service

--- src/app/todo-module/services/todo.service.ts
+++ src/app/todo-module/services/todo.service.ts
@@ -7,33 +7,27 @@ import {map} from "rxjs/operators";
 @Injectable()
 export class TodoService {
   protected baseUrl = 'http://localhost:3000'
-
   constructor(protected http: HttpClient) {
   }
-
   getTodoList() {
     return this.http.get<Todo[]>(`${this.baseUrl}/todo`)
   }
-
   deleteTodo(todoId: number) {
     return this.http.delete<{}>(`${this.baseUrl}/todo/${todoId}`).pipe(
       map(res => todoId)
     )
   }
-
   updateTodo(todoId: number, todo: Todo): Observable<Todo> {
     return this.http.patch<Todo>(`${this.baseUrl}/todo/${todoId}`, {
       ...todo,
       id: todoId
     })
   }
-
   addTodo(todo: Todo) {
     return this.http.post<Todo>(`${this.baseUrl}/todo`, {
       ...todo,
     })
   }
-
   checkUnique(value: string, queryParam: string, entityId?: number) {
     return this.http.get<Todo[]>(`${this.baseUrl}/todo`, {
       params: {
